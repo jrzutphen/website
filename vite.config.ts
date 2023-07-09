5300b0d2be835f3e528298c834c10b919c0721b6
Luka Leer <luka@jrzutphen.nl>
2023-07-09 12:43:06 +0200

    🐛 Fix service worker evaluation failure
    Signed-off-by: Luka Leer <luka@jrzutphen.nl>

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import { imagetools } from "vite-imagetools";
 export default defineConfig({
 	plugins: [sveltekit(), imagetools()],
 	define: {
+		// What this does is replace all instances of process.env.NODE_ENV with 'production' or 'development' depending on the environment.
+		// Since we replace it with a string, we need to wrap it in quotes.
 		"process.env.NODE_ENV":
-			process.env.NODE_ENV === "production" ? "production" : "development"
+			process.env.NODE_ENV === "production"
+				? "'production'"
+				: "'development'"
 	}
 });
