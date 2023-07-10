d4cef2ec6a47327e38e4e4fa051726e716f9df55
Luka Leer <luka@jrzutphen.nl>
2023-07-10 20:58:21 +0200

    🐛 Fix Lexend font-face import settings
    Signed-off-by: Luka Leer <luka@jrzutphen.nl>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<em>Italic text</em>
+<div style="font-weight: 100">Increasingly bolder text</div>
+<div style="font-weight: 150">Increasingly bolder text</div>
+<div style="font-weight: 200">Increasingly bolder text</div>
+<div style="font-weight: 250">Increasingly bolder text</div>
+<div style="font-weight: 300">Increasingly bolder text</div>
+<div style="font-weight: 350">Increasingly bolder text</div>
+<div style="font-weight: 400">Increasingly bolder text</div>
+<div style="font-weight: 450">Increasingly bolder text</div>
+<div style="font-weight: 500">Increasingly bolder text</div>
+<div style="font-weight: 550">Increasingly bolder text</div>
+<div style="font-weight: 600">Increasingly bolder text</div>
+<div style="font-weight: 650">Increasingly bolder text</div>
+<div style="font-weight: 700">Increasingly bolder text</div>
+<div style="font-weight: 750">Increasingly bolder text</div>
+<div style="font-weight: 800">Increasingly bolder text</div>
+<div style="font-weight: 850">Increasingly bolder text</div>
+<div style="font-weight: 900">Increasingly bolder text</div>
+<div style="font-weight: 950">Increasingly bolder text</div>
+<div style="font-weight: 1000">Increasingly bolder text</div>
